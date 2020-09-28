6dd947fe0146de5e9f4cc4cf9989c59d380a15f1
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2020-09-28 09:50:47 +0700

    Penerapan controller action

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function() {
-    return 'Hello World';
-});
+Route::get('/hello', 'WelcomeController@hello');
