7be940fcacfc22dca8431a987540a90bbabc3bbf
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2020-10-01 12:25:03 +0700

    tambah dummy

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Article;
+use Faker\Generator as Faker;
+
+$factory->define(Article::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(),
+        'content' => $faker->realText(250),
+        'featured_image' => $faker->imageUrl(750, 300, 'cats', true)
+    ];
+});
